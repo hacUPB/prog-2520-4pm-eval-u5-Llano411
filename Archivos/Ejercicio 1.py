2d12415e1fc9ad6d3b2c6106db1636ab723220cb
Alejandro llano <alejandrollano999@gmail.com>
2025-10-21 17:29:42 -0500

    Contexto de archivos

---
+++ Archivos/Ejercicio 1.py
@@ -0,0 +1,18 @@
+import random
+# Solicitamos al usuario el nombre del archivo a crear
+nombre_archivo = input("Ingrese el nombre del archivo de texto: ")
+
+# Usamos 'with' para crear el contexto y escribir datos en el archivo 
+with open(nombre_archivo, 'w') as archivo:
+    # Solicitamos al usuario los datos a escribir en el archivo
+    datos = random.uniform(0,5)
+    archivo.write(str(datos))
+
+# Ahora usamos 'with' para crear el contexto donde leer los datos del archivo
+with open(nombre_archivo, 'r') as archivo:
+    contenido = archivo.read()
+    print("Contenido del archivo:")
+    print(contenido)
+    
+
+# Tarea: investigar codigo "try" de python y traer un ejemplo 
